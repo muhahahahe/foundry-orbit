f02bd327cde8b39064090de11a74d18270419a56
muhahahahe <muhahahahe@gmail.com>
2022-05-07 21:19:04 +0200

    Add files via upload

---
+++ src/classes.js
@@ -0,0 +1,145 @@
+class Orbit {
+    constructor() {
+        this.tokens = [];
+        this.characters = [];
+        this.executeOrbit = false;
+        this.started = false;
+        this.orbitStartOrientation = game.settings.get(MODULE_NAME_ORBIT, "orbitStartOrientation");
+        this.orbitSpeed = game.settings.get(MODULE_NAME_ORBIT, "orbitSpeed");
+        this.orbitStartDate = game.settings.get(MODULE_NAME_ORBIT, "orbitStartDate");
+        this.DEBUG = false;
+    }
+
+    static get() {
+        return new Orbit();
+    }
+
+    mapOrbitPaths() {
+        let orbitCircles = canvas.drawings.placeables.filter((p) => p.data.text == "Orbit");
+        this.tokens = [];
+        canvas.tokens.placeables.filter((t) => t.document.getFlag(MODULE_NAME_ORBIT, "enableOrbit")).forEach((t) => {
+            let tokenDrawing;
+            for (let circles of orbitCircles) {
+                let c = PIXI.Circle(this.adjustCirclePoints(circles));
+                if (c.contains(t.position)) tokenDrawing = c;
+            }
+            this.tokens.push({
+                tokenDocument: t,
+                visitedPositions: [`${t.x}-${t.y}`],
+                orbitCircle: tokenDrawing 
+            });
+        });
+        this.characters = canvas.tokens.placeables.filter((t) => t.actor && (t.actor?.type == "character" || t.actor?.type == "char" || t.actor?.hasPlayerOwner));
+    }
+
+    async orbitDelay() {
+        setTimeout(() => {
+            this.executeOrbit = true;
+        }, /*game.settings.get(MODULE_NAME_ORBIT, "orbitDelay")*/ 250);
+    }
+
+    orbitStart() {
+        this.mapTokens();
+        this.orbitDelay();
+        canvas.app.ticker.add(this.orbitCompute);
+    }
+
+    orbitCompute() {
+        if (_orbit.executeOrbit && !game.paused && _orbit.started) {
+            let perfStart, perfEnd;
+            if (_orbit.DEBUG) perfStart = performance.now();
+            _orbit.mapTokens();
+            _orbit.executeOrbit = false;
+            _orbit.orbitDelay();
+            for (let token of _orbit.tokens) {
+                if (token.tokenDocument._controlled) continue;
+                let newPosition = _orbit.getNextPosition(token);
+                updates.push({
+                    _id: token.tokenDocument._id,
+                    x: newPosition.x,
+                    y: newPosition.y
+                });
+                token.visitedPositions.push(`${newPosition.x}-${newPosition.y}`);
+            }
+            canvas.scene.updateEmbeddedDocuments("Token", updates);
+
+            if (_orbit.DEBUG) {
+                perfEnd = performance.now();
+                console.log(`Orbit compute took: ${perfEnd - perfStart}ms, FPS: ${Math.round(canvas.app.ticker.FPS)}`);
+            }
+        }
+    }
+
+    getNextPosition(token) {
+        //calculate the next position on the circle from the current position
+        let g = canvas.dimensions.size;
+        let position = [
+            {
+              x: token.x + g,
+              y: token.y,
+              center: { x: token.center.x + g, y: token.center.y },
+            },
+            {
+              x: token.x - g,
+              y: token.y,
+              center: { x: token.center.x - g, y: token.center.y },
+            },
+            {
+              x: token.x,
+              y: token.y + g,
+              center: { x: token.center.x, y: token.center.y + g },
+            },
+            {
+              x: token.x,
+              y: token.y - g,
+              center: { x: token.center.x, y: token.center.y - g },
+            },
+          ];
+        let nextPosition = position[Math.floor(Math.random() * position.length)];
+        if (token.orbitCircle) {
+            let c = token.orbitCircle;
+            let p = nextPosition;
+            let cX = c.x;
+            let cY = c.y;
+            let pX = p.x;
+            let pY = p.y;
+            let cCenterX = c.center.x;
+            let cCenterY = c.center.y;
+            let pCenterX = p.center.x;
+            let pCenterY = p.center.y;
+
+            let a = Math.abs(cX - pX);
+            let b = Math.abs(cY - pY);
+            c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            a = Math.abs(cCenterX - pCenterX);
+            b = Math.abs(cCenterY - pCenterY);
+            cCenter = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            a = Math.abs(cX - cCenterX);
+            b = Math.abs(cY - cCenterY);
+            cX = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            a = Math.abs(pX - pCenterX);
+            b = Math.abs(pY - pCenterY);
+            pX = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            a = Math.abs(pX - cX);
+            b = Math.abs(pY - cY);
+            pX = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            if (cX + cCenter + pX > c + cCenter + pCenter) {
+                return nextPosition;
+            } else {
+                return this.getNextPosition(token);
+            }
+        } else {
+            return nextPosition;
+        }
+    }
+
+    adjustCirclePoints(circle) {
+        let globalpoints = [];
+        for (let i = 0; i < circle.points.length; i++) {
+            let point = circle.points[i];
+            globalpoints.push(circle.position.x + point.x);
+            globalpoints.push(circle.position.y + point.y);
+        }
+        return globalpoints;
+    }
+}
--- src/config.js
+++ src/config.js
@@ -5,29 +5,32 @@ Hooks.once("socketlib.ready", () => {
 });
 
 Hooks.on("getSceneControlButtons", (controls, scene, user) => {
-    if (!game.user.isGM) return;
-    if (!_orbit) _orbit = Orbit.get();
-    basictools = controls.find((c) => c["name"] == "token").tools;
-    basictools.push({
-        name: "Orbit",
-        icon: "fas fa-sun",
-        active: _orbit.started,
-        title: game.i18n.localize("orbit.tools.orbitToggle.hint"),
-        onClick: (toggle) => {
-            _orbit.started = toggle;
-        },
-        toggle: true,
-    }, {
-        button: true,
-        visible: true,
-        icon: "fas fa-draw-circle",
-        name: "remapOrbitPaths",
-        title: game.i18n.localize("orbit.tools.remapOrbitPaths.hint"),
-        onClick: () => {
-            _orbit.mapTokenAndOrbitPaths();
-            _orbit.resetPathIndex();
-        }
-    });
+    if (game.user.isGM) {
+        if (!_orbit) _orbit = Orbit.get();
+        if (!_pathOrbit) _pathOrbit = PathOrbiter.get();
+        basictools = controls.find((c) => c["name"] == "token").tools;
+        basictools.push({
+            name: "Orbit",
+            icon: "fas fa-sun",
+            active: _orbit.started,
+            title: game.i18n.localize("orbit.tools.orbitToggle.hint"),
+            onClick: (toggle) => {
+                _orbit.started = toggle;
+                _pathOrbit.started = toggle;
+            },
+            toggle: true,
+        }, {
+            button: true,
+            visible: true,
+            icon: "fas fa-draw-circle",
+            name: "remapOrbitPaths",
+            title: game.i18n.localize("orbit.tools.remapOrbitPaths.hint"),
+            onClick: () => {
+                _pathOrbit.mapTokensAndOrbits();
+                _pathOrbit.resetPathIndex();
+            }
+        });
+    }
 });
 
 Hooks.on("init", () => {
@@ -48,6 +51,7 @@ Hooks.on("init", () => {
             console.log(`Orbit start orientation changed to ${value}`);
         }
     });
+
     game.settings.register(MODULE_NAME_ORBIT, "orbitStartDate", {
         name: game.i18n.localize("orbit.settings.orbitStartDate.name"),
         hint: game.i18n.localize("orbit.settings.orbitStartDate.hint"),
@@ -59,6 +63,7 @@ Hooks.on("init", () => {
             console.log(`Orbit start date changed to ${value}`);
         }
     });
+
     game.settings.register(MODULE_NAME_ORBIT, "orbitSpeed", {
         name: game.i18n.localize("orbit.settings.orbitSpeed.name"),
         hint: game.i18n.localize("orbit.settings.orbitSpeed.hint"),
@@ -75,4 +80,86 @@ Hooks.on("init", () => {
             console.log(`Orbit speed changed to ${value}`);
         }
     });
+
+    libWrapper.register(MODULE_NAME_ORBIT,"Token.prototype.animateMovement", _orbitAnimateMovement, "OVERRIDE")
+});
+
+Hooks.on("renderTokenConfig", (app, html, data) => {
+    if (!game.user.isGM) return;
+    let toggleHTML= 
+    `<div class="form-group">
+        <label>${game.i18n.localize("orbit.tokenConfig.enableOrbit.name")}</label>
+        <input type="checkbox" name="flags.${MODULE_NAME_ORBIT}.enableOrbit" data-dtype="Boolean">
+    </div>
+    <div class="form-group">
+        <label>${game.i18n.localize("orbit.tokenConfig.makeOrbit.name")}</label>
+        <select type="checkbox" name="flags.${MODULE_NAME_ORBIT}.makeOrbit" data-dtype="Boolean">
+    <div class="form-group">
+        <label>${game.i18n.localize("orbit.tokenConfig.orbitPathName.name")}</label>
+        <input type="text" name="flags.${MODULE_NAME_ORBIT}.orbitPathName" value="">
+        <label>${game.i18n.localize("orbit.tokenConfig.orbitNodeIndex.hint")}</label>
+        <input type="text" name="flags.${MODULE_NAME_ORBIT}.orbitNodeIndex" value="">
+    </div>`;
+    const lockrotation = html.find("input[name='lockRotation']");
+    const formGroup = lockrotation.closest(".form-group");
+    formGroup.after(toggleHTML);
+    html.find(`input[name='flags.${MODULE_NAME_ORBIT}.enableOrbit']`)[0].checked = app.token.getFlag(MODULE_NAME_ORBIT, "enableOrbit") || false;
+    html.find(`input[name='flags.${MODULE_NAME_ORBIT}.orbitPathName']`)[0].value = app.token.getFlag(MODULE_NAME_ORBIT, "orbitPathName") || "";
+    html.find(`input[name='flags.${MODULE_NAME_ORBIT}.orbitNodeIndex']`)[0].value = app.token.getFlag(MODULE_NAME_ORBIT, "orbitNodeIndex") || 0;
+});
+
+Hooks.on("createDrawing", () => {
+    if (game.user.isGM) {
+        _orbit.mapTokens();
+        _pathOrbit.mapTokensAndOrbits();
+    }
+});
+
+Hooks.on("updateDrawing", () => {
+    if (game.user.isGM) {
+        _orbit.mapTokens();
+        _pathOrbit.resetPathIndex();
+    }
+});
+
+Hooks.on("deleteDrawing", () => {
+    if (game.user.isGM) {
+        _orbit.mapTokens();
+        _pathOrbit.resetPathIndex();
+    }
+});
+
+Hooks.on("createToken", () => {
+    if (game.user.isGM) {
+        _orbit.mapTokens();
+        _pathOrbit.mapTokensAndOrbits();
+    }
+});
+
+Hooks.on("deleteToken", () => {
+    if (game.user.isGM) {
+        _orbit.mapTokens();
+        _pathOrbit.mapTokensAndOrbits();
+    }
+});
+
+Hooks.on("updateToken", async (tokend, updates) => {
+    if (game.user.isGM && updates.flags?.orbit && (updates.flags.orbit.orbitPathIndex == undefined || updates.flags.orbit.pathID == undefined)) {
+        let token = canvas.tokens.get(tokend.id)
+        if (token.document.getFlag(MODULE_NAME_ORBIT, "makeOrbit")) {
+            let pathName = token.document.getFlag(MODULE_NAME_ORBIT, "orbitPathName");
+            let pathGroup = canvas.drawings.placeables.filter((d) => {
+                if (d.data.text == pathName) {
+                    return d;
+                }
+            });
+            let pathID
+            if (pathGroup[0] != undefined) {
+                pathID = pathGroup[0].id;
+            }
+            await token.document.setFlag(MODULE_NAME_ORBIT, "pathID", pathID);
+        }
+        _pathOrbit.mapTokensAndOrbits();
+        _orbit.mapTokens();
+    }
 });
